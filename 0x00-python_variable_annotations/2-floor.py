abecb526bc07a55c1983f4a5f54bdb191e0e2693
Simamnkele <smambo85@gmail.com>
2024-03-14 12:16:03 +0200

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Below module returns the floor of the float argument.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Function returns the floor of a float arg
+    """
+    return (int(n))
