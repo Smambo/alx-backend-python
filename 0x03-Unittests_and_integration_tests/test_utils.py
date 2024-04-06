1712e58f7a576043a406eeb1b10b45c64983d5df
Simamnkele <smambo85@gmail.com>
2024-04-06 17:04:34 +0200

    Created a class that parameterized a unittest

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Import modules for utils module"""
+
+import unittest
+from parameterized import parameterized
+from typing import Dict, Tuple, Union
+from unittest.mock import Mock, patch
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test for the access_nested_map function"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """Test output for access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
